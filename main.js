acef70d2b17f1fa148ac19620f1e4c8d24cbc8db
Giôvanni_BS <99855424+GiovanniBandeira@users.noreply.github.com>
2024-09-04 17:19:18 -0300

    Atualização

--- main.js
+++ main.js
@@ -1,3 +1,39 @@
-document.getElementById("likedin").addEventListener("click", function() {
-    window.open("https://www.linkedin.com/in/giovanni-bandeira", "_blank");
-})
+function topScroll(){
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+function sobreScroll(){
+    window.scrollTo({
+        top: 950,
+        behavior: 'smooth'
+    })
+}
+
+function projetosScroll(){
+    window.scrollTo({
+        top: 1800,
+        behavior: 'smooth'
+    })
+}
+
+function contatoScroll(){
+    window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+    })
+}
+
+function linksClick(){
+    if(document.getElementById('linkedin')){
+        window.open("https://www.linkedin.com/in/giovanni-bandeira", '_black')
+    }
+    else if(document.getElementById('gits')){
+        window.open()
+    }
+    else if(document.getElementById('email')){
+        window.open("mailto:giovanni.bandeira.sousa@gmail.com")
+    }
+}
